11bb16a88c369317cc5db0d904b34b1f61625504
Samyak Mohelay <samyakmohelay@gmail.com>
2023-05-15 11:03:54 +0530

    first commit

---
+++ kneeoa/config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb+srv://samyakmohelay:'+ encodeURIComponent('4ttCA7pTLsXuDijb') + '@cluster0.2ygb1aq.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: dbPassword
+};
+ 
